071a49667a8c2c3573f5106c651c75da695bce36
shahzain alam <shahzaina354@gmail.com>
2025-01-22 06:47:42 +0000

    chng

--- final.js
+++ final.js
@@ -1,35 +1,44 @@
-
 let URL = "https://icanhazdadjoke.com/";
 
-function fetchDadJokes(){
-return new Promise((resolve, reject) => {
+function fetchDadJokes() {
+  return new Promise((resolve, reject) => {
     fetch(URL, {
       headers: { Accept: "application/json" },
-    }).then((res) => {
-        console.log(res, "res");
+    })
+      .then((res) => {
         if (res.ok) {
           return res.json();
         } else {
           reject("failed to fetch");
         }
-      }).then((data) => {
-          resolve(data)
-        console.log(data, "data");
-      }).catch((err)=>{
-              reject(err)
+      })
+      .then((data) => {
+        resolve(data);
+      })
+      .catch((err) => {
+        reject(err);
       });
   });
 }
 
-
 const clickBtn = document.getElementById("dadJokes");
 
-clickBtn.addEventListener("click", ()=>{
-    fetchDadJokes().then((data)=>{
-        console.log(data, "data");
-        document.getElementById("main_content").innerText = data.joke
-    }).catch((err)=>{
-        document.getElementById("main_content").innerText = err
+clickBtn.addEventListener("click", async () => {
+  const jokesContainer = document.getElementById("main_content");
+  jokesContainer.innerHTML = ""; // Clear previous jokes
 
-    })
-})
+  try {
+    const jokes = [];
+    for (let i = 0; i < 5; i++) { // Fetch 5 jokes
+      const jokeData = await fetchDadJokes();
+      jokes.push(jokeData.joke);
+    }
+    jokes.forEach((joke) => {
+      const jokeElement = document.createElement("p");
+      jokeElement.innerText = joke;
+      jokesContainer.appendChild(jokeElement);
+    });
+  } catch (err) {
+    jokesContainer.innerText = err;
+  }
+});
